f94e009043a1f03f036e47f0a83bb1740f5beb0c
David Moser <david@theworkingcomputer.com>
2015-07-10 13:58:48 -0400

    added allthethings.py boilerplate

---
+++ allthethings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python2.7
+
+from UserDict import UserDict
+
+
+class LinkedList(UserDict):
+    pass
+    """Use a dict to implement the Linked List data structure."""
+
+
+class DoubleLinkedList(UserDict):
+    pass
+    """Use a dict to implement a doubly linked list data structure."""
+
+
+class FingerTree(UserDict):
+    pass
+    """Use dicts to implement finger trees in python."""
